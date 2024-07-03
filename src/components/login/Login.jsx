e82e3dd0cc5f3f6d7ff94b00e4de318fc03f179f
paula <gaby_pau@live.com.ar>
2024-07-03 15:58:03 -0300

    reemplace el formulario de login por el de hook form

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,97 +2,139 @@ import { useState } from "react";
 
 import { ViewPassword } from "./ViewPassword";
 
+import { Box, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+
 export const Login = ({ setLoggedIn, setIsRegister }) => {
-	const [userNameLogin, setUserNameLogin] = useState("");
-	const [password, setPassword] = useState("");
 	const [typePassword, setTypePassword] = useState("password");
 
-	// console.log(userNameLogin);
-
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+	} = useForm();
+	// console.log(errors);
 	const usuario = {
 		nombre: "Paula",
 		contraseña: "123",
 	};
 
-	const handleFormSubmit = (e) => {
-		e.preventDefault();
+	const onSubmit = (data) => {
+		// console.log(data);
 
 		const datos = {
-			nombre: userNameLogin,
-			contraseña: password,
+			name: data.name,
+			password: data.password,
 		};
 
 		if (
-			usuario.nombre === datos.nombre &&
-			usuario.contraseña === datos.contraseña
+			usuario.nombre === datos.name &&
+			usuario.contraseña === datos.password
 		) {
 			setLoggedIn(true);
 		}
-		if (
-			usuario.nombre != datos.nombre ||
-			usuario.contraseña != datos.contraseña
-		) {
+		if (usuario.nombre != datos.name || usuario.contraseña != datos.password) {
 			alert("Los datos ingresados no coinciden con ningún usuario registrado.");
 		}
 
-		if (datos.nombre === "" || datos.contraseña === "") {
+		if (datos.name === "" || datos.password === "") {
 			alert("Por favor complete todos los campos");
 		}
 	};
 	return (
-		<div className="mt-[14%]">
+		<Box mt="14%">
 			<h2 className="text-2xl text-center mb-8">Iniciar Sesión</h2>
-			<form
-				onSubmit={handleFormSubmit}
-				className="border border-black rounded-2xl p-5 max-w-[300px] mx-auto mb-8"
+
+			<Box
+				as="form"
+				border="1px"
+				borderColor="black"
+				borderRadius="1rem"
+				p="1.25rem"
+				maxW="300px"
+				mx="auto"
+				mb="2rem"
+				onSubmit={handleSubmit(onSubmit)}
 			>
-				<div className="mb-5 relative">
-					<label htmlFor="userName" className="block mb-[10px]">
+				<FormControl
+					mb={5}
+					position="relative"
+					isInvalid={errors.name ? true : false}
+				>
+					<FormLabel display="block" mb="10px">
 						Nombre Usuario
-					</label>
-					<input
+					</FormLabel>
+					<Input
 						type="text"
 						placeholder="Maria Antonieta"
-						className="w-full border border-black rounded-md px-1 py-2"
-						value={userNameLogin}
-						onChange={(e) => setUserNameLogin(e.target.value)}
+						w="full"
+						border="1px"
+						borderColor="black"
+						borderRadius="md"
+						px={2}
+						py={2}
+						{...register("name", {
+							required: "El nombre de usuario es obligatorio.",
+						})}
 					/>
-				</div>
-				<div className="mb-5 relative">
-					<label htmlFor="password" className="block mb-[10px]">
+					<ErrorMessage
+						errors={errors}
+						name="name"
+						render={({ message }) => <p className="text-red-600">{message}</p>}
+					/>
+				</FormControl>
+
+				<FormControl
+					mb={5}
+					position="relative"
+					isInvalid={errors.password ? true : false}
+				>
+					<FormLabel display="block" mb="10px">
 						Contraseña
-					</label>
-					<input
+					</FormLabel>
+					<Input
 						type={typePassword}
 						className="w-full border border-black rounded-md px-1 py-2"
-						value={password}
-						onChange={(e) => {
-							setPassword(e.target.value);
-						}}
+						w="full"
+						border="1px"
+						borderColor="black"
+						borderRadius="md"
+						px={2}
+						py={2}
+						{...register("password", {
+							required: "La contraseña es obligatoria.",
+						})}
 					/>
 					<ViewPassword
 						typePassword={typePassword}
 						setTypePassword={setTypePassword}
 					/>
-				</div>
+					<ErrorMessage
+						errors={errors}
+						name="password"
+						render={({ message }) => <p className="text-red-600">{message}</p>}
+					/>
+				</FormControl>
 
 				<input
 					type="submit"
 					value="Iniciar Sesión"
 					className="border px-2 py-1 bg-gray-200 rounded block mx-auto cursor-pointer"
 				/>
-			</form>
+			</Box>
 
 			<p className="text-center">
 				No tienes cuenta?{" "}
-				<span
+				<Box
+					as="span"
 					className="text-[#1d1dcd] cursor-pointer underline"
 					onClick={() => setIsRegister(false)}
 				>
 					Regístrate
-				</span>
+				</Box>
 				.
 			</p>
-		</div>
+		</Box>
 	);
 };
